b947bfe601f573af6b71bd50dd4141fbd43ca6e6
Hansem Ro <hansemro@outlook.com>
2022-04-09 00:07:40 -0700

    Add image and clipboard paste support via GDClip
    This commit introduces image support via ImageStroke and image-paste
    support via GDClip GDNative library. ImageStroke extends Sprite to track
    visibility of images and to cooperate with SelectionTool.
    
    All SelectionTool features are supported (including selection, movement,
    copy, paste, duplication, and deletion), but note that SelectionTool
    copy does not copy to clipboard.
    
    Save/load feature supports images as well, but the new save format will
    not be compatible with prior versions.

--- lorien/ProjectManager/Serializer.gd
+++ lorien/ProjectManager/Serializer.gd
@@ -6,55 +6,71 @@ class_name Serializer
 
 # -------------------------------------------------------------------------------------------------
 const BRUSH_STROKE = preload("res://BrushStroke/BrushStroke.tscn")
+const IMAGE_STROKE = preload("res://BrushStroke/ImageStroke.tscn")
 const COMPRESSION_METHOD = File.COMPRESSION_DEFLATE
 const POINT_ELEM_SIZE := 3
 
-const VERSION_NUMBER := 0
+const VERSION_NUMBER := 1
 const TYPE_BRUSH_STROKE := 0
 const TYPE_ERASER_STROKE := 1
+const TYPE_IMAGE_STROKE := 2
 
 # -------------------------------------------------------------------------------------------------
 static func save_project(project: Project) -> void:
 	var start_time := OS.get_ticks_msec()
-	
+
 	# Open file
 	var file := File.new()
 	var err = file.open_compressed(project.filepath, File.WRITE, COMPRESSION_METHOD)
 	if err != OK:
 		print_debug("Failed to open file for writing: %s" % project.filepath)
 		return
-	
+
 	# Meta data
 	file.store_32(VERSION_NUMBER)
 	file.store_pascal_string(_dict_to_metadata_str(project.meta_data))
-	
+
 	# Stroke data
 	for stroke in project.strokes:
 		# Type
-		if stroke.eraser:
-			file.store_8(TYPE_ERASER_STROKE)
-		else:
-			file.store_8(TYPE_BRUSH_STROKE)
-		
-		# Color
-		file.store_8(stroke.color.r8)
-		file.store_8(stroke.color.g8)
-		file.store_8(stroke.color.b8)
-		
-		# Brush size
-		file.store_16(int(stroke.size))
-		
-		# Number of points
-		file.store_16(stroke.points.size())
-		
-		# Points
-		var p_idx := 0
-		for p in stroke.points:
-			# Add global_position offset which is != 0 when moved by move tool; but mostly it should just add 0
-			file.store_float(p.x + stroke.global_position.x)
-			file.store_float(p.y + stroke.global_position.y)
-			file.store_8(stroke.pressures[p_idx])
-			p_idx += 1
+		if "BrushStroke" in stroke.name:
+			if stroke.eraser:
+				file.store_8(TYPE_ERASER_STROKE)
+			else:
+				file.store_8(TYPE_BRUSH_STROKE)
+
+			# Color
+			file.store_8(stroke.color.r8)
+			file.store_8(stroke.color.g8)
+			file.store_8(stroke.color.b8)
+
+			# Brush size
+			file.store_16(int(stroke.size))
+
+			# Number of points
+			file.store_16(stroke.points.size())
+
+			# Points
+			var p_idx := 0
+			for p in stroke.points:
+				# Add global_position offset which is != 0 when moved by move tool; but mostly it should just add 0
+				file.store_float(p.x + stroke.global_position.x)
+				file.store_float(p.y + stroke.global_position.y)
+				file.store_8(stroke.pressures[p_idx])
+				p_idx += 1
+		elif "ImageStroke" in stroke.name:
+			# Type
+			file.store_8(TYPE_IMAGE_STROKE)
+			# Width
+			file.store_32(stroke.get_rect().size[0])
+			# Height
+			file.store_32(stroke.get_rect().size[1])
+			# Position
+			file.store_float(stroke.global_position.x)
+			file.store_float(stroke.global_position.y)
+			# RGBA8 Image data
+			for byte in stroke.texture.get_data().get_data():
+				file.store_8(byte)
 
 	# Done
 	file.close()
@@ -70,55 +86,75 @@ static func load_project(project: Project) -> void:
 	if err != OK:
 		print_debug("Failed to load file: %s" % project.filepath)
 		return
-	
+
 	# Clear potential previous data
 	project.strokes.clear()
 	project.meta_data.clear()
-	
+
 	# Meta data
 	var _version_number := file.get_32()
 	var meta_data_str = file.get_pascal_string()
 	project.meta_data = _metadata_str_to_dict(meta_data_str)
 	var canvas_color: Color = ProjectMetadata.get_canvas_color_from_dict(project.meta_data)
-	
+
 	# Brush strokes
 	var stroke_index := 0
 	while true:
-		var brush_stroke: BrushStroke = BRUSH_STROKE.instance()
-		
 		# Type
 		var type := file.get_8()
-		var is_eraser_stroke := (type == TYPE_ERASER_STROKE)
-		if is_eraser_stroke:
-			brush_stroke.eraser = true
-			project.eraser_stroke_indices.append(stroke_index)
-		
-		# Color
-		var r := file.get_8()
-		var g := file.get_8()
-		var b := file.get_8()
-		brush_stroke.color = Color(r/255.0, g/255.0, b/255.0, 1.0)
-		
-		# v0 just saved eraser stroke colors as black. Now we need it to be the actual background color...so just set it 
-		# here for all eraser strokes no matter the version. Doesn't hurt.
-		if is_eraser_stroke:
-			brush_stroke.color = canvas_color
-		
-		# Brush size
-		brush_stroke.size = file.get_16()
-			
-		# Number of points
-		var point_count := file.get_16()
+		if type == TYPE_BRUSH_STROKE or type == TYPE_ERASER_STROKE:
+			var brush_stroke: BrushStroke = BRUSH_STROKE.instance()
+			var is_eraser_stroke := (type == TYPE_ERASER_STROKE)
+			if is_eraser_stroke:
+				brush_stroke.eraser = true
+				project.eraser_stroke_indices.append(stroke_index)
 
-		# Points
-		for i in point_count:
-			var x := file.get_float()
-			var y := file.get_float()
-			var pressure := file.get_8()
-			brush_stroke.points.append(Vector2(x, y))
-			brush_stroke.pressures.append(pressure)
-		project.strokes.append(brush_stroke)
-		
+			# Color
+			var r := file.get_8()
+			var g := file.get_8()
+			var b := file.get_8()
+			brush_stroke.color = Color(r/255.0, g/255.0, b/255.0, 1.0)
+
+			# v0 just saved eraser stroke colors as black. Now we need it to be the actual background color...so just set it 
+			# here for all eraser strokes no matter the version. Doesn't hurt.
+			if is_eraser_stroke:
+				brush_stroke.color = canvas_color
+
+			# Brush size
+			brush_stroke.size = file.get_16()
+
+			# Number of points
+			var point_count := file.get_16()
+
+			# Points
+			for i in point_count:
+				var x := file.get_float()
+				var y := file.get_float()
+				var pressure := file.get_8()
+				brush_stroke.points.append(Vector2(x, y))
+				brush_stroke.pressures.append(pressure)
+			project.strokes.append(brush_stroke)
+		elif type == TYPE_IMAGE_STROKE:
+			var image_stroke: ImageStroke = IMAGE_STROKE.instance()
+			# Width
+			var width := file.get_32()
+			# Height
+			var height := file.get_32()
+			# Position
+			image_stroke.position.x = file.get_float()
+			image_stroke.position.y = file.get_float()
+			# RGBA8 Image data
+			var pbarray = PoolByteArray()
+			for _i in range(0, width*height*4):
+				pbarray.append(file.get_8())
+			var image = Image.new()
+			image.create_from_data(width, height, false, Image.FORMAT_RGBA8, pbarray)
+			print("Size from image: %s" % str(image.get_size()))
+			var texture = ImageTexture.new()
+			texture.create_from_image(image)
+			image_stroke.set_texture(texture)
+			project.strokes.append(image_stroke)
+
 		# are we done yet?
 		stroke_index += 1
 		if file.get_position() >= file.get_len()-1 || file.eof_reached():
