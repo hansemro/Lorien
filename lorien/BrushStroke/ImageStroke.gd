09d89ad8a40bf448c2088f5df26bd7ca6eebd78c
Hansem Ro <hansemro@outlook.com>
2022-04-19 00:58:31 -0700

    ImageStroke: cleanup commented code and ignore collider

--- lorien/BrushStroke/ImageStroke.gd
+++ lorien/BrushStroke/ImageStroke.gd
@@ -38,26 +38,5 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	visible = false
 
 # -------------------------------------------------------------------------------------------------
-#func _to_string() -> String:
-#	return "Size: %d, Points: %s" % [size, points]
-
-# -------------------------------------------------------------------------------------------------
 func enable_collider(enable: bool) -> void:
 	pass
-	# Remove current collider
-	var collider = get_node_or_null(COLLIDER_NODE_NAME)
-	if collider != null:
-		remove_child(collider)
-		collider.queue_free()
-	
-	# Create new collider
-	if enable:
-		var body := StaticBody2D.new()
-		body.name = COLLIDER_NODE_NAME
-		var idx := 0
-		var col := CollisionObject2D.new()
-		var shape := RectangleShape2D.new()
-		shape.extents = get_rect().size
-		print("rect shape extents: " + str(shape.extents))
-		body.add_child(col)
-		add_child(body)
